73fd2574e6e950edf98b6a74e86810693745d6fa
edu-bn <eduardo.barreran@gmail.com>
2024-04-26 19:53:19 -0400

    ventana

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Crear una instancia de la clase Tk, que representa la ventana principal de la aplicación
+ventana = tk.Tk()
+
+# Opcional: configurar propiedades de la ventana, como su tamaño y título
+ventana.geometry("900x600")  # Establece el tamaño de la ventana a 400x300 píxeles
+ventana.title("AFD")  # Establece el título de la ventana
+
+# Código adicional para añadir widgets, botones, etiquetas, etc.
+
+# Bucle principal de la aplicación para que la ventana permanezca abierta
+ventana.mainloop()
