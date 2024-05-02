2565a63d08f35856a231bdadcd9c22faf346206e
ivan <107969090+free-hornet@users.noreply.github.com>
2024-05-01 20:25:33 -0400

    ola

--- simulador_afd.py
+++ simulador_afd.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from clase_AFD import AFD
+from clase_afd import AFD
 
 def ventana_estados(ventana):
     """Crea y muestra la ventana para el ingreso de estado inicial y estados finales de un AFD.
